84637c5ecc73c4300868d78cea881ddd43710c5d
debraaaho <101456624+debraaaho@users.noreply.github.com>
2022-08-30 10:45:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cfc5qkudrtpp.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hoyancheng-employee"
+customregion = "us-east-1"
